0a21af46be7aad535a221b5ab3a59c9c9cff8274
GuyRofe <guyrrofe@gmail.com>
2022-09-29 11:05:31 +0300

    initial setup with server and index

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    );
+    next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'web', 'index.html'));
+});
+
+module.exports = app;
