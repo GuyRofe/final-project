26552c44f9f847be8e63503ad768521bf2e886a1
GuyRofe <guyrrofe@gmail.com>
2022-09-29 22:40:12 +0300

    handle edge cases

--- src/public/js/buyer-registration.js
+++ src/public/js/buyer-registration.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     const registrationForm = $('#registrationForm');
     const usernameInput = $('#buyerRegistrationUsernameInput');
     const passwordInput = $('#buyerRegistrationPasswordInput');
+    const formError = $('#formError');
 
     registrationForm.on('submit', function(e) {
         e.preventDefault();
@@ -10,6 +11,9 @@ $(document).ready(function() {
         const password = passwordInput.val();
 
         if (!username || !password) {
+            formError.removeClass("d-none");
+            formError.text('Please complete the form');
+
             return;
         }
 
@@ -24,6 +28,13 @@ $(document).ready(function() {
             data: postDataString,
             contentType: "application/json",
             dataType: "json",
+            error: function(response) {
+                const responseMessage = response.responseText;
+                const responseMessageObject = JSON.parse(responseMessage);
+
+                formError.removeClass("d-none");
+                formError.text(responseMessageObject.message);
+            }
         });
     });
 });
